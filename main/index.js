5e8126ea22b003dd5f175b1e5052060e9419715b
Mazir <nguyenmanh10a7@gmail.com>
2023-06-09 18:04:28 +0700

    hoho

--- main/index.js
+++ main/index.js
@@ -4,3 +4,15 @@ $('.slick-testmonials').slick({
     prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
   });
+
+const btnMenu = document.querySelector('.menu')
+const btnClose = document.querySelector('.menu-close')
+const headerMobile = document.querySelector('.header-mobile')
+
+btnMenu.onclick = () => {
+  headerMobile.classList.add('header-mobile__active')
+}
+
+btnClose.onclick = () => {
+  headerMobile.classList.remove('header-mobile__active')
+}
